6d0fb7a8a183d261f8777f55033049b5efc42c62
karthikkancharla2002 <you@example.com>
2021-03-01 20:03:30 +0530

    homepage modifications

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -7,14 +7,14 @@ const Footer = () => {
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
+            <h5 className="title">Contact Us</h5>
             <p>
               Here you can use rows and columns here to organize your footer
               content.
             </p>
           </MDBCol>
           <MDBCol md="6">
-            <h5 className="title">Links</h5>
+            <h5 className="title">Social Media Handles</h5>
             <ul>
               <li className="list-unstyled">
                 <a href="#!">Link 1</a>
@@ -30,11 +30,12 @@ const Footer = () => {
               </li>
             </ul>
           </MDBCol>
+          
         </MDBRow>
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.iitdh.ac.in"> IIT Dharwad </a>
         </MDBContainer>
       </div>
     </MDBFooter>
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button,Navbar,Nav,NavDropdown,Form,FormControl } from 'react-bootstrap';
-
+import logo from "../../assets/logo.png";
 
 const Header = () => (
   <Navbar bg="light" expand="lg" >
-  <Navbar.Brand href="#home">IIT</Navbar.Brand>
+  
+  <img src={logo} style={{width:100, marginTop: -7}} />
+  <Navbar.Brand href="#home">Indian Institute of Technology, Dharwad</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto" >
@@ -12,6 +14,8 @@ const Header = () => (
       <Nav.Link href="#link">OFFICE BEARERS</Nav.Link>
       <Nav.Link href="#link">COUNCILS</Nav.Link>
       <Nav.Link href="#link">NOTICES</Nav.Link>
+      <Nav.Link href="#link">STUDENT BODIES</Nav.Link>
+      <Nav.Link href="#link">TEAM</Nav.Link>
       <Nav.Link href="#link">CONTACT</Nav.Link>
     </Nav>
     
