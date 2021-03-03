be968680a0f7f9354dfa524b9c3c742811729cf3
Ati <190020009@iitdh.ac.in>
2021-03-03 10:54:20 +0530

    nav bar bug fixed

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Button, Navbar, Nav, NavDropdown, } from 'react-bootstrap';
+import { Button, Navbar, Nav, NavDropdown, Container, } from 'react-bootstrap';
+import { StyleSheet, css } from 'aphrodite';
 import logo from "../../assets/logo.png";
-import './style.css';
 
 const Header = () => (
-  <Navbar  expand="lg"  >
-    <img src={logo} style={{ width: 70, marginTop: -7, marginLeft: 10 }} />
+  <Navbar  
+  expand="lg" 
+  collapseOnSelect
+  fixed = {'top'}
+  style = {{
+    backgroundColor:'aqua', 
+    paddingLeft:'2%', 
+    paddingRight:'2%', 
+    width:'100%'}}
+  >
+    <img src={logo} style={{ maxWidth:'60px'}} />
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="ml-auto" >
-        <Nav.Link href="/">HOME</Nav.Link>
+      <Nav className="ml-auto">
+        <Nav.Link href="/"  >HOME</Nav.Link>
         <Nav.Link href="/office">OFFICE BEARERS</Nav.Link>
         <NavDropdown title="COUNCILS" id="basic-nav-dropdown">
           <NavDropdown.Item href="/technical">TECHNICAL COUNCILS</NavDropdown.Item>
@@ -17,15 +26,11 @@ const Header = () => (
           <NavDropdown.Item href="/sports">SPORTS COUNCILS</NavDropdown.Item>
         </NavDropdown>
         <Nav.Link href="/notices">NOTICES</Nav.Link>
-        <Nav.Link href="#link">STUDENT BODIES</Nav.Link>
         <Nav.Link href="/calendar">CALENDAR</Nav.Link>
-        <Nav.Link href="#link">TEAM</Nav.Link>
         <Nav.Link href="/contact">CONTACT</Nav.Link>
       </Nav>
-
     </Navbar.Collapse>
   </Navbar>
-
 );
 
 export default Header;
