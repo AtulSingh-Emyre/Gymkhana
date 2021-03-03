1c4a5f185b94d924e28a38ac242cc34a92ea0384
karthikkancharla2002 <you@example.com>
2021-03-03 05:55:03 +0530

    some changes

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -4,8 +4,8 @@ import logo from "../../assets/logo.png";
 import './style.css';
 
 const Header = () => (
-  <Navbar  expand="lg" >
-
+  <Navbar  expand="lg">
+    
     <img src={logo} style={{ width: 70, marginTop: -7, marginLeft: 10 }} />
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
