72db77ab038a271161d08860883b7ece7d478b92
Your Name <harrithha64@gmail.com>
2021-03-04 18:17:54 +0530

    PrevCouncil

--- frontend/src/components/PreviousCouncilMems/index.tsx
+++ frontend/src/components/PreviousCouncilMems/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 interface IProps {
     user : {
-        gsTech: string,
-        gsAcad: string,
+        gsAcadUG: string,
+        gsAcadPG: string,
+        gsCult: string;
+        gsHos: string;
+        gsTech: string;
+        gsSports: string;
 
     }
 }
@@ -13,7 +17,13 @@ const PreviousCouncilMems = (prop : IProps) => {
     
     return (
         <div style={container} >
-          asdfsdf:  {prop.user.gsAcad}
+General Secretary Academic Affairs (PG):	<br />  {prop.user.gsAcadUG}
+General Secretary Academic Affairs (UG):	<br />   {prop.user.gsAcadPG}
+General Secretary Cultural Affairs:	<br />           {prop.user.gsCult}
+General Secretary Hostel Affairs:	<br />           {prop.user.gsHos}
+General Secretary Technical Affairs:	<br />       {prop.user.gsTech}
+General Secretary Sports Affairs:                     {prop.user.gsSports}
+          
         </div>
     );
 }
