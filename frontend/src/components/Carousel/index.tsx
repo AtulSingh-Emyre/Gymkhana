439a6a55bab69e2ebe966ed7dffb141253f07aa0
karthikkancharla2002 <you@example.com>
2021-03-06 15:51:30 +0530

    council pages changes

--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -41,25 +41,25 @@ const Carouselcomp = (prop : IProps) => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
+            src={prop.images.image2}
+            alt={prop.images.caption2}
           />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>{prop.images.title2}</h3>
+            <p>{prop.images.caption2}</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
+            src={prop.images.image3}
+            alt={prop.images.caption3}
           />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          <h3>{prop.images.title3}</h3>
+            <p>{prop.images.caption3}</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
