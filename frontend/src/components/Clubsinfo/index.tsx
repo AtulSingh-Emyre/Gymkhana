c0a39e1264cece3d321ad2d46e4165a4522fcac2
Ati <190020009@iitdh.ac.in>
2021-03-06 14:36:35 +0530

    error fixes

--- frontend/src/components/Clubsinfo/index.tsx
+++ frontend/src/components/Clubsinfo/index.tsx
@@ -3,7 +3,6 @@ import clubdata from '../../assets/clubdetails.json';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 interface IProps {
-
     club: {
         clubname: string;
     }
@@ -15,16 +14,17 @@ return(
 <div>
 <h1>Clubs Overview</h1>
             <br/>
-    {clubdata.sports.map((item, i) => (
-
-        
+    {clubdata.sports.map((item, i) => {
+        const path = item.logo;
+        // + item.logo;
+        return (
         <div key={i}>
-
             <h2>{item.clubname} Club</h2>
-            <img src={item.logo} alt={item.clubname} style={{float:'left',}}/>
+            <img src={require('./../../assets/'+path)} alt={item.clubname} style={{float:'left',}}/>
             <p>{item.clubdesc}</p>
         </div>
-      ))}
+    )}
+    )}
 
 </div>
 </Jumbotron>
