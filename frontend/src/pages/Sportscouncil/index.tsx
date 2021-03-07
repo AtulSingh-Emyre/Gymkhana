70467cb317c934f097eda975d1b2e516a85a2c2c
Ashish Akash <ashishakash333@gmail.com>
2021-03-07 15:22:59 +0530

    improving look of council pages

--- frontend/src/pages/Sportscouncil/index.tsx
+++ frontend/src/pages/Sportscouncil/index.tsx
@@ -5,7 +5,7 @@ import Footer from '../../components/Footer';
 import Clubsinfo from '../../components/Clubsinfo';
 import BootstrapTable from 'react-bootstrap-table-next';
 import clubdata from '../../assets/clubdetails.json'
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 import './style.css';
 
 const logo = require('../../assets/interiitsmeet.jpg');
@@ -33,19 +33,19 @@ const Sportscouncil: React.FC = () => {
     <>
       <Header />
 
-      <h1 style={{ textAlign: 'center' }}>Sports Council</h1>
+     
       <Carouselcomp images={{
         image1: logo, title1: "", caption1: "hello",
         image2: logo, title2: "", caption2: "",
         image3: logo, title3: "", caption3: "",
       }} />
       <br />
-      <Jumbotron className='rounded-pill'>
+      <Container style={{padding:'10px'}}>
         <div>
-          <h3>About</h3>
+          <h3 style={{ textDecoration: 'underline' }}>About</h3>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
-      </Jumbotron>
+      </Container>
       <hr></hr>
       <Clubsinfo club={clubdata.sports} />
       <hr></hr>
