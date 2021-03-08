61026fa5fd8dab8e9ecaa25f1068b112cb354e70
siddahwala02 <siddharthwala02@gmail.com>
2021-03-08 21:39:09 +0530

    font office bearers

--- frontend/src/components/PreviousCouncilMems/index.tsx
+++ frontend/src/components/PreviousCouncilMems/index.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import { BiChevronsDown } from "react-icons/bi";
+import './styles.css'
 
 interface Ipropss {
     user : {
@@ -18,19 +19,25 @@ interface Ipropss {
 const PreviousCouncilMems = (props : Ipropss) => {
     const [Hover, setHover] = useState(false);
     return (
-        <div 
-        style={{...container,  background: Hover? '#FFEE58' :'#FFC400' , }} 
-        onMouseEnter={()=>setHover(true)} 
+        <div
+        style={{...container,  background: Hover? '#FFEE58' :'#FFC400' , }}
+        onMouseEnter={()=>setHover(true)}
         onMouseLeave={()=>setHover(false)}
         >
             <h5 style={{textAlign:'center'}}> {props.user.year} <BiChevronsDown color={'black'} size={30} />  </h5>
-            
-                {props.user.gsTech && Hover?<>General Secretary, Technical Affairs: {props.user.gsTech} <br/></>:<></>}
-                    {props.user.gsAcad&& Hover?<> General Secretary Academic Affairs: {props.user.gsAcad}<br/></>:<></>}
-                    {props.user.gsCult&& Hover?<>General Secretary Cultural Affairs: {props.user.gsCult}<br/></>:<></>}
-                    {props.user.gsSports&& Hover? <>General Secretary Sports Affairs: {props.user.gsSports}<br/></>:<></>}
-                    {props.user.gsHos&& Hover? <>General Secretary Hostel Affairs: {props.user.gsHos}<br/></>:<></>} 
-                    {props.user.gsMess&& Hover? <>General Secretary Mess Affairs: {props.user.gsMess}<br/></>:<></>}
+            <div className='dropDown'>  
+            {props.user.gsTech && Hover?<>General Secretary, Technical Affairs: {props.user.gsTech} <br/></>:<></>}
+                  {props.user.gsAcad&& Hover?<> General Secretary Academic Affairs: {props.user.gsAcad}<br/></>:<></>}
+                  {props.user.gsCult&& Hover?<>General Secretary Cultural Affairs: {props.user.gsCult}<br/></>:<></>}
+                  {props.user.gsSports&& Hover? <>General Secretary Sports Affairs: {props.user.gsSports}<br/></>:<></>}
+                  {props.user.gsHos&& Hover? <>General Secretary Hostel Affairs: {props.user.gsHos}<br/></>:<></>}
+                  {props.user.gsMess&& Hover? <>General Secretary Mess Affairs: {props.user.gsMess}<br/></>:<></>}
+                  </div>
+
+
+
+
+
         </div>
     );
 }
