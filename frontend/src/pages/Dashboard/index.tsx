47dfad79fe12374ac00ab987929679734b540513
Ati <190020009@iitdh.ac.in>
2021-03-10 08:09:01 +0530

    header tweaks

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,15 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Homepage from '../../components/Homepage';
+
+// const Header = lazy(() => import('../../components/Header'))
+// const Footer = lazy(() => import('../../components/Footer'))
+// const Homepage = lazy(() => import('../../components/Homepage'))
+
 
 
 const Dashboard: React.FC = () => {
   return (
     <div style={{minWidth:'100%'}} >
       <Header active={{home:true}} />
+      
       <Homepage/>
+      
       <Footer />
     </div>
   );
