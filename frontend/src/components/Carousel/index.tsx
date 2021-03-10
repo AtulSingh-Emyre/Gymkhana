baf17339b0071d78398708e4a3a46193715be867
Ashish Akash <ashishakash333@gmail.com>
2021-03-10 19:33:52 +0530

    removing bugs

--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -3,35 +3,36 @@ import { Carousel, CarouselItem } from 'react-bootstrap';
 // import logo from '../../assets/interiitsmeet.jpg';
 
 interface IProps {
-  images : {
-      image1: string,
-      title1: string,
-      caption1: string,
-      image2: string,
-      title2: string,
-      caption2: string,
-      image3: string,
-      title3: string,
-      caption3: string,
+  images: {
+    image1: string,
+    title1: string,
+    caption1: string,
+    image2: string,
+    title2: string,
+    caption2: string,
+    image3: string,
+    title3: string,
+    caption3: string,
   }
 }
 
 
 
 
-const Carouselcomp = (prop : IProps) => {
+const Carouselcomp = (prop: IProps) => {
 
   var image1 = prop.images.image1
   return (
-    
-    <div>
+
+    <div >
       <Carousel fade>
-        <Carousel.Item interval={1000}>
+        <Carousel.Item interval={100}>
           <img
             className="d-block w-100"
             // `url(${require(`../../assets/logoc.jpg`)})`
             src={prop.images.image1}
             alt={prop.images.caption1}
+            style={{ height: '600px' }}
           />
           <Carousel.Caption>
             <h3>{prop.images.title1}</h3>
@@ -43,6 +44,7 @@ const Carouselcomp = (prop : IProps) => {
             className="d-block w-100"
             src={prop.images.image2}
             alt={prop.images.caption2}
+            style={{ height: '600px' }}
           />
 
           <Carousel.Caption>
@@ -55,10 +57,11 @@ const Carouselcomp = (prop : IProps) => {
             className="d-block w-100"
             src={prop.images.image3}
             alt={prop.images.caption3}
+            style={{ height: '600px' }}
           />
 
           <Carousel.Caption>
-          <h3>{prop.images.title3}</h3>
+            <h3>{prop.images.title3}</h3>
             <p>{prop.images.caption3}</p>
           </Carousel.Caption>
         </Carousel.Item>
