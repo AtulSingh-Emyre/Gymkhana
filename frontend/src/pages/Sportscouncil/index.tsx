58419b1d7d1745d96be001abfd212eebe5ccb588
karthikkancharla2002 <karthikkancharla2002@gmail.com>
2021-03-30 22:35:42 +0530

    sports council new page

--- frontend/src/pages/Sportscouncil/index.tsx
+++ frontend/src/pages/Sportscouncil/index.tsx
@@ -1,22 +1,39 @@
 import React, { useState, useEffect } from 'react';
-const Sportscouncil: React.FC = () => {
-  const columns = [
-     {
-      dataField: 'clubname',
-      text: 'Sports Clubs'
-    }, {
-      dataField: 'secyname',
-      text: 'Club Secretary'
-    }, {
-      dataField: 'Contactdetails',
-      text: 'Contact'
-    }
-  ];
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import Header from '../../SportsCouncilComponents/Header/index';
+import './style.css';
+import Homestyle from '../../SportsCouncilComponents/Home/index';
+import {Card, Button} from 'react-bootstrap';
+import Footer from '../../components/Footer/index';
 
+
+const Sportscouncil: React.FC = () => {
   return (
-    <>
-    </>
+    <div className="mainhome">
+    <Header/>
+    <div>
+    <Homestyle/>
+
+    </div>
+    
+    {/* <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card> */}
+<Footer/>
+    </div>
+   
+  
+
   );
+
 };
 
 export default Sportscouncil;
