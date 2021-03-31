c72afb57369be6f79a7c0fd628f06019ed0db5e5
Ashish Akash <ashishakash333@gmail.com>
2021-03-31 16:49:05 +0530

    fixing errors

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -17,33 +17,33 @@ const Admin: React.FC = () => {
   const [LoggedIn, setLoggedIn] = useState(false);
   const [EventsData, setEventsData] = useState([]);
   const [loading, setLoading] = useState(false);
-  
+
 
 
   const updateData = async () => {
     const clubs = [clubnames.aiclub];
   }
   const login = async (identity: string) => {
-    try{
-        setLoading(true);
-        const resp = await AuthRepository.getAdminPerms(identity);
-        console.log(resp);
-        if(resp.data.success) {
-          setLoggedIn(true);
-          setLoading(false);
-        }
+    try {
+      setLoading(true);
+      const resp = await AuthRepository.getAdminPerms(identity);
+      console.log(resp);
+      if (resp.data.success) {
+        setLoggedIn(true);
+        setLoading(false);
       }
-    catch(e) {
-      console.log(e);  
-      setLoading(false);    
+    }
+    catch (e) {
+      console.log(e);
+      setLoading(false);
     }
   }
 
-  const responseGoogle = (response:any) => {
+  const responseGoogle = (response: any) => {
     const user = {
-        name: response.profileObj.name,
-        email: response.profileObj.email,
-        avatar: response.profileObj.imageUrl,
+      name: response.profileObj.name,
+      email: response.profileObj.email,
+      avatar: response.profileObj.imageUrl,
     }
     console.log(user);
     const iden = user.email.split('@')[0];
@@ -52,17 +52,17 @@ const Admin: React.FC = () => {
 
   return (
     <>
-    <Container>
-     {
-       LoggedIn? <><EventsCalendar />
-       <Admininputform /></> :<div style={{marginLeft:50, width:'100%'}}>  <GoogleLogin
-                clientId="92821589740-n027pnhb5hla7cfjc76emtvg6hvkhd4u.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
-                buttonText="LOGIN WITH GOOGLE"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-            /> </div>  
-     } 
-     </Container>
+      <Container>
+        {
+          LoggedIn ? <><EventsCalendar editable={true} />
+            <Admininputform /></> : <div style={{ marginLeft: 50, width: '100%' }}>  <GoogleLogin
+              clientId="92821589740-n027pnhb5hla7cfjc76emtvg6hvkhd4u.apps.googleusercontent.com" //CLIENTID NOT CREATED YET
+              buttonText="LOGIN WITH GOOGLE"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+            /> </div>
+        }
+      </Container>
     </>
   );
 };
