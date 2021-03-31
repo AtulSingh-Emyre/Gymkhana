ba0396e37e28c77c639755d77c9b597c7e2e99d4
Ashish Akash <ashishakash333@gmail.com>
2021-03-31 09:54:45 +0530

    tweaking around

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter,MDBIcon } from "mdbreact";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBIcon } from "mdbreact";
 import './style.css';
 import SocialButtonsPage from '../socialmediaicons';
 import { Container } from "react-bootstrap";
-import {GrLocation} from "react-icons/gr";
-import {GrMailOption} from "react-icons/gr";
-import {FiPhoneCall} from "react-icons/fi";
+import { GrLocation } from "react-icons/gr";
+import { GrMailOption } from "react-icons/gr";
+import { FiPhoneCall } from "react-icons/fi";
 
 
 const Footer = () => {
@@ -14,17 +14,29 @@ const Footer = () => {
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6" >
-            <h5 className="title">Contact Us</h5>
+            <div style={{ position: 'sticky' }}>
+              <h5 className="text-uppercase mb-4 mt-3 font-weight-bold">
+                Contact
+            </h5>
+              <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px" }} />
+            </div>
             <p>
-         
-            
-            <a href="https://goo.gl/maps/WZiRcratqas"><GrLocation style={{backgroundColor:'white'}} />   Indian Institute of Technology Dharwad, Karnataka, India-580011<br /></a>
-            <GrMailOption color={'white'} style={{backgroundColor:'white'}} />  pro@iitdh.ac.in<br />
-            <FiPhoneCall/>  +91 836 2212 839
+              <i className="fa fa-home mr-3" /> New York, NY 10012, US
+            </p>
+            <p>
+              <i className="fa fa-envelope mr-3" /> info@example.com
+            </p>
+            <p>
+              <i className="fa fa-phone mr-3" /> + 01 234 567 88
+            </p>
+            <p>
+              <i className="fa fa-print mr-3" /> + 01 234 567 89
             </p>
           </MDBCol>
           <MDBCol md="3" xs='10' >
-            <h5 className="title">Quick Links</h5>
+            <h5 className="title">QUICK LINKS</h5>
+            <br />
+            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px" }} />
             <Container style={{ paddingBottom: 10 }}>
               <div><a href="https://www.iitdh.ac.in/">IIT Dharwad</a></div>
               <div><a href="https://parsec.iitdh.ac.in/">Parsec</a></div>
@@ -33,11 +45,13 @@ const Footer = () => {
               <div><a href="https://oss2019.github.io/">OSS</a></div>
               <div><a href="https://www.iitdh.ac.in/iic/">IIC</a></div>
             </Container>
-           
+
           </MDBCol>
           <MDBCol md="3">
             <div style={{ margin: 'auto', alignItems: 'center', width: '100%', height: '100%' }}>
-              <h5 className="title" style={{ margin: 'auto' }}>Our Handles</h5>
+              <h5 className="title" style={{ margin: 'auto' }}>OUR HANDLES</h5>
+              <br />
+              <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px" }} />
               <div style={{ margin: 'auto', alignItems: 'center' }}>
                 <SocialButtonsPage />
               </div>
