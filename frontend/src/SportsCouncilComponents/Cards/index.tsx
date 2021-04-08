d94634cbf9125d43058bda3f86400a6f3c56ad7b
Samy <200020040@iitdh.ac.in>
2021-04-09 01:24:47 +0530

    updated

--- frontend/src/SportsCouncilComponents/Cards/index.tsx
+++ frontend/src/SportsCouncilComponents/Cards/index.tsx
@@ -2,55 +2,129 @@ import React from 'react'
 // @ts-ignore
 import { CardView } from 'react-card-with-image';
 import 'react-card-with-image/dist/index.css';
-import logo from '../../assets/interiitsmeet.jpg';
 import './style.css';
 
-const Cards = () => {
-  const items = [
-    {
-      id: 1,
-      header: 'Lorem ipsum',
-      description:
-        'dolor sit amet, consectetur adipiscing elit. Sed tempus nunc et tincidunt lobortis. Aliquam placerat, justo sit amet mattis molestie, ipsum nisi congue turpis, in imperdiet nisi nisl sit amet arcu. Donec euismod eu ante quis elementum. Maecenas commodo erat',
-      image: logo
-    },
-    {
-      id: 2,
-      header: 'Sed cursus',
-      description:
-        'in metus quis tempor. Donec at venenatis magna, vel fringilla dui. Curabitur id gravida ipsum. Donec at mollis massa. Nullam metus elit, pret',
-      image: logo
-    },
-    {
-      id: 3,
-      header: 'Sed fermentum',
-      description:
-        'condimentum purus, non sagittis massa faucibus id. Sed finibus convallis lectus eu fringilla. Proin lacinia sem vitae nunc consectetur, a faucibus orci ultricie',
-      image: logo
-    },
-    {
-      id: 4,
-      header: 'Proin et urna',
-      description:
-        'vitae neque fermentum fringilla. Proin bibendum sollicitudin aliquet. Fusce id magna aliquam, pulvinar metus vitae, bibendum felis.',
-      image: logo
-    },
-    {
-      id: 5,
-      header: 'Cras leo velit',
-      description:
-        'finibus id eros eu, commodo sollicitudin lacus. Nunc semper enim nec est viverra, at pharetra orci lobortis. Nulla facilisi. Sed non lectus nunc.',
-      image: logo
-    }
-  ]
+interface club {
+  id: number;
+  clubname: string;
+  memblink: string;
+  logo: string;
+  contactdetails: string;
+  clubdesc: string;
+}
+
+interface Iprops{
+  info : club;
+}
+
+const info = [
+  {
+      id: "1",
+      clubname: "Athletic Club",
+      secyname:  "Shivam Kumar",
+      memblink: "google",
+      logo: "logo.png",
+      Contactdetails: "123456 123456",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "2",
+      clubname: "Badminton Club",
+      secyname:  "Saloni Singh",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "3",
+      clubname: "Basketball Club",
+      secyname: "Vaibhav Mishra",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "4",
+      clubname: "Board Games Club",
+      secyname:  "Aditya Kalyani",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "5",
+      clubname: "Cricket Club",
+      secyname:  "Mohit Jorwal",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "6",
+      clubname: "Football Club",
+      secyname:  "Aneesh Bendale",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "7",
+      clubname: "Hockey Club",
+      secyname:  "Karu Jagadeesh",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "8",
+      clubname: "Table Tennis Club",
+      secyname:  "Shivaram H G",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  
+  {
+      id: "9",
+      clubname: "Tennis Club",
+      secyname:  "Mohammed Huzaif Barkati",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "10",
+      clubname: "Volleyball Club",
+      secyname:  "Cheryala Rohith",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
+      id: "11",
+      clubname: "Weightlifting Club",
+      secyname:  "Balsher Singh",
+      memblink: "google",
+      logo: "logo.png",
+      clubdesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  }
+]
+
+const Clubs = () => {
   return (
-    <CardView
-      items={items}
-      activeColor='#1FC1E9'
-      imageHeight='400px'
-      imageWidth='600px'
-    />
+    <div style={{...stile}}>
+
+    </div>
   )
 }
 
-export default Cards
+const stile: React.CSSProperties = {
+  width: '450px',
+  height: '300px',
+  backgroundColor: 'white',
+  borderRadius: '10px',
+  position:'absolute',
+  transform: 'translate(-50%, -50%)'
+}
+
+export default Clubs;
