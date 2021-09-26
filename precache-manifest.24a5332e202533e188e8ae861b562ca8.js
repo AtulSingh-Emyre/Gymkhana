beac1957b950dd3f9586dba673e9630267774203
Ashish Akash <ashishakash333@gmail.com>
2021-09-26 13:47:23 +0530

    Updates

---
+++ precache-manifest.24a5332e202533e188e8ae861b562ca8.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d4655ecde3438161341acb1709199d",
+    "url": "/Gymkhana/index.html"
+  },
+  {
+    "revision": "9fa8fa2a74940550568d",
+    "url": "/Gymkhana/static/css/2.b5e0f7d1.chunk.css"
+  },
+  {
+    "revision": "71cdb2eeae3339b94c86",
+    "url": "/Gymkhana/static/css/main.dc20a912.chunk.css"
+  },
+  {
+    "revision": "9fa8fa2a74940550568d",
+    "url": "/Gymkhana/static/js/2.3be21975.chunk.js"
+  },
+  {
+    "revision": "7967269e54bfc7373301a09e46ac9be6",
+    "url": "/Gymkhana/static/js/2.3be21975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71cdb2eeae3339b94c86",
+    "url": "/Gymkhana/static/js/main.fa490511.chunk.js"
+  },
+  {
+    "revision": "24400322c0dfcd496fc1",
+    "url": "/Gymkhana/static/js/runtime-main.489eb8af.js"
+  },
+  {
+    "revision": "176c96851f6bf4a3616d7b990339c64c",
+    "url": "/Gymkhana/static/media/AI_secy.176c9685.png"
+  },
+  {
+    "revision": "4ee6e334d30b7ea2ef44bb46bc383707",
+    "url": "/Gymkhana/static/media/Calvin_Jerome_Suares_Live_Show.4ee6e334.png"
+  },
+  {
+    "revision": "c31a1d762ca01489b335ec054fc799ca",
+    "url": "/Gymkhana/static/media/Inter_Branch_Chess.c31a1d76.jpeg"
+  },
+  {
+    "revision": "fcf77a90d2b00a7a2f9c26386eb2aab7",
+    "url": "/Gymkhana/static/media/Robotics.fcf77a90.png"
+  },
+  {
+    "revision": "c64639362622b84b1539f4970a46afa3",
+    "url": "/Gymkhana/static/media/Robotics_secy.c6463936.jpg"
+  },
+  {
+    "revision": "abe8b8e6757b0a8864442d5bc3785a9d",
+    "url": "/Gymkhana/static/media/Summer_Of_Innovation.abe8b8e6.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Gymkhana/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Gymkhana/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "fcb5c4cbaa3f282a49bbf90c755134ec",
+    "url": "/Gymkhana/static/media/codeGeass.fcb5c4cb.png"
+  },
+  {
+    "revision": "4a8380c42ad19b8bbf7b2a1e8b4a657b",
+    "url": "/Gymkhana/static/media/code_secy.4a8380c4.jpg"
+  },
+  {
+    "revision": "6d50b2c68196770a888761ec97e42e4e",
+    "url": "/Gymkhana/static/media/councilLogo.6d50b2c6.png"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/Gymkhana/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/Gymkhana/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/Gymkhana/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/Gymkhana/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/Gymkhana/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/Gymkhana/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/Gymkhana/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/Gymkhana/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/Gymkhana/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/Gymkhana/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/Gymkhana/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/Gymkhana/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/Gymkhana/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/Gymkhana/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/Gymkhana/static/media/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "c1d14449e62194d660abb7865c860ae5",
+    "url": "/Gymkhana/static/media/gs-Acad.c1d14449.jpg"
+  },
+  {
+    "revision": "d5675a8eff88b1bc9021b2ad588e9e79",
+    "url": "/Gymkhana/static/media/gs-Cult.d5675a8e.jpg"
+  },
+  {
+    "revision": "d63662314587329f7a39d853ad933a8f",
+    "url": "/Gymkhana/static/media/gs-Hostel.d6366231.jpg"
+  },
+  {
+    "revision": "1c3e6178f86582e59be2d1d025dbd6d6",
+    "url": "/Gymkhana/static/media/gs-Mess.1c3e6178.png"
+  },
+  {
+    "revision": "a4698f570155ad0a48ea1d4bde0b3ebc",
+    "url": "/Gymkhana/static/media/gs-Sports.a4698f57.jpg"
+  },
+  {
+    "revision": "91d1c595b2b3ed88dc2fd7d4307b3c89",
+    "url": "/Gymkhana/static/media/gs-Tech.91d1c595.jpg"
+  },
+  {
+    "revision": "5c7240cc631d79d7f282be7c8b3b36e0",
+    "url": "/Gymkhana/static/media/iitdhlogoLight.5c7240cc.png"
+  },
+  {
+    "revision": "2b77c3f02354d2a60ccd4c2761b67a1a",
+    "url": "/Gymkhana/static/media/img1.2b77c3f0.jpg"
+  },
+  {
+    "revision": "a79a5c014b7998e0925aab72250e4a3f",
+    "url": "/Gymkhana/static/media/img2.a79a5c01.jpg"
+  },
+  {
+    "revision": "d5a4c7818abd5f6c1ebce3bab5c52184",
+    "url": "/Gymkhana/static/media/img3.d5a4c781.jpg"
+  },
+  {
+    "revision": "25d73f60e0666d7eb60c9631b06392c5",
+    "url": "/Gymkhana/static/media/img4.25d73f60.jpg"
+  },
+  {
+    "revision": "19f5bc234c578a1a114867cf4e5a08d9",
+    "url": "/Gymkhana/static/media/img5.19f5bc23.jpg"
+  },
+  {
+    "revision": "ebaea60e54e0391465c5128fc3186190",
+    "url": "/Gymkhana/static/media/img6.ebaea60e.jpg"
+  },
+  {
+    "revision": "1a88af52214815bdfcc52ae5f7bd4952",
+    "url": "/Gymkhana/static/media/img7.1a88af52.jpg"
+  },
+  {
+    "revision": "2b50cb9e639fc1f69f1ad397477c8f37",
+    "url": "/Gymkhana/static/media/img8.2b50cb9e.jpg"
+  },
+  {
+    "revision": "63aac462d0df4bb0a228c4ccfb12f436",
+    "url": "/Gymkhana/static/media/img9.63aac462.jpg"
+  }
+]);
